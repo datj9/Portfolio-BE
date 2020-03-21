d41441a9fce361261a76a657670921da5e4edc2d
Dat Nguyen <tandat198@gmail.com>
2020-03-21 07:27:19 +0700

    enable cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./config");
 const mongoURI = config.mongoURI;
@@ -17,6 +18,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api", require("./routes/api"));
+app.use("/api", cors(), require("./routes/api"));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
