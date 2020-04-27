ab1a616c13d6c3eee146848659b922c57cdc5e50
Dat Nguyen <tandat198@gmail.com>
2020-04-27 12:21:29 +0700

    update

--- routes/api/user/index.js
+++ routes/api/user/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { validateCreateUser } = require("../../../middleware/validation/user");
 const userController = require("./controller");
 
-router.get("/signup", validateCreateUser, userController.createUser);
-router.get("/signin", userController.signin);
+router.post("/signup", validateCreateUser, userController.createUser);
+router.post("/signin", userController.signin);
 
 module.exports = router;
